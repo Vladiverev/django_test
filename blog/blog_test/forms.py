672b48c083fc43b0cd22990363ca7bb183d68256
Vladiverev <vladiverev@gmail.com>
2018-07-31 17:10:00 +0300

    commetns

--- blog/blog_test/forms.py
+++ blog/blog_test/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Post, Comment
 
 
@@ -14,4 +15,14 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ('author', 'text',)
+        fields = ('text', 'parent')
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email', 'password1', 'password2', )
--- blog/blog_test/models.py
+++ blog/blog_test/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from mptt.models import MPTTModel, TreeForeignKey
+
 
 # Create your models here.
-from django.utils import timezone
 
 
 class Post(models.Model):
@@ -9,30 +11,37 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+        default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+        blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
-
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
+
+class Comment(MPTTModel):
     post = models.ForeignKey('blog_test.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE,  null=True, blank=True, related_name='children', db_index=True)
 
     def approve(self):
         self.approved_comment = True
         self.save()
 
+    class MPTTMeta:
+        # comments on one level will be ordered by date of creation
+        order_insertion_by = ['created_date']
+
     def __str__(self):
         return self.text
+
+
+
+
